de6e650bad98864ce86b1996f3459b7eceb754c3
loyhauchun <loyhauchun@gmail.com>
2026-04-17 17:17:46 +0500

    Update Sale.js

--- models/Sale.js
+++ models/Sale.js
@@ -37,25 +37,41 @@ const saleSchema = new mongoose.Schema(
       type: [saleItemSchema],
       default: []
     },
+
+    subtotalAmount: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+
+    discountAmount: {
+      type: Number,
+      default: 0
+    },
+
     totalAmount: {
       type: Number,
       required: true,
       default: 0
     },
+
     paymentType: {
       type: String,
       enum: ["cash", "card", "transfer", "debt"],
       default: "cash"
     },
+
     seller: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       default: null
     },
+
     sellerName: {
       type: String,
       default: ""
     },
+
     note: {
       type: String,
       default: ""
