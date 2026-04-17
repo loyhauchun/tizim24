27876550635884a673e1a0e9115dc1fac0cadd52
loyhauchun <loyhauchun@gmail.com>
2026-04-17 17:51:09 +0500

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -7,47 +7,57 @@ const productSchema = new mongoose.Schema(
       required: true,
       trim: true
     },
+
     category: {
       type: String,
       default: "",
       trim: true
     },
+
     unit: {
       type: String,
       enum: ["dona", "kg", "metr", "m2", "m3"],
       required: true
     },
+
     purchasePrice: {
       type: Number,
       default: 0
     },
+
     salePrice: {
       type: Number,
       required: true,
       default: 0
     },
+
     minPrice: {
       type: Number,
       default: 0
     },
+
     stock: {
       type: Number,
       default: 0
     },
+
     sku: {
       type: String,
       default: "",
       trim: true
     },
+
     allowDebt: {
       type: Boolean,
       default: true
     },
+
     description: {
       type: String,
       default: "",
       trim: true
     },
+
     isActive: {
       type: Boolean,
       default: true
